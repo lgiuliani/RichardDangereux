8552a3b0180ec88ea990c907e6ac2afb5e5f2fbf
Laurent GIULIANI <l_giuliani@hotmail.com>
2020-04-16 17:11:14 +0200

    Add South America assets

---
+++ assets/South America/Bank 2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="Bank 2" tilewidth="8" tileheight="8" tilecount="256" columns="4">
+ <image source="Bank2.bmp" width="32" height="512"/>
+</tileset>
